ec0fc4664fbac83ddd6307fcecd2901fc3c837c8
David Ukelere <david.ukelere291@gmail.com>
2020-07-01 13:15:46 +0100

    Setup update task

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import *
 
@@ -10,5 +10,22 @@ from .forms import *
 def index(request):
     tasks = Task.objects.all()
     form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
     context = {'tasks': tasks, 'forms': form}
     return render(request, 'tasks/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'tasks/update_task.html', context)
